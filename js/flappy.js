8474b7dc03677269fa1f19afda93ae80426d6ff9
Leonardo Abreu <leonardo.abreu@simbioseventures.com>
2020-06-18 12:23:46 -0300

    Feat: add folder to js file

--- js/flappy.js
+++ js/flappy.js
@@ -11,4 +11,10 @@ function Barreira(reversa = false) {
   const corpo = novoElemento('div', 'corpo')
   this.elemento.appendChild(reversa ? corpo : borda)
   this.elemento.appendChild(reversa ? borda : corpo)
+
+  this.setAltura = altura => corpo.style.height = '${altura}px'
 }
+
+const b = new Barreira(false)
+b.setAltura(200)
+document.querySelector('[wm-flappy]').appendChild(b.elemento)
